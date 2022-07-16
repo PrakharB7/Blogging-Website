c102dce72243a5b43344b865aeb68b5d40750a54
unknown <prakharbhatnagar03@gmail.com>
2022-07-16 11:43:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const homeStartingContent = "Welcome to Daily Jounral where you can write amazing content and also read amazing contact.";
+const aboutContent = "Our mission drives us to do everything possible needed to make something that can help the content writers who are obscure with regards to how to do and how to manage their abilities.";
+
+const contactContent = "Contact Us at dailyjournal@gmail.com";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
+
+app.get("/", function(req, res){
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post ({
+
+    title: req.body.postTitle,
+ 
+    content: req.body.postBody
+ 
+  });
+
+  post.save(function(err){
+
+    if (!err){
+ 
+      res.redirect("/");
+ 
+    }
+ 
+  });
+ 
+ 
+
+});
+
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
